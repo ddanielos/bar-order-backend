f73d5b49305735f84a1010ac5db6fa268c4e402b
Dany Daniel <68875920+ddanielos@users.noreply.github.com>
2023-05-13 07:11:29 +0000

    first commit MVC

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
+/*
 const apiRouter = require('./app/routes/api');
 
 require('dotenv').config()
@@ -13,12 +16,33 @@ require('./db')
 
 const PORT = process.env.PORT || 3001
 
-/* app.get('/', (req, res) => {
-  res.send('Hola, mundo!');
-}); */
-
 app.use('/api', apiRouter);
 
 app.listen(PORT, ()=>{
   console.log('Servidor iniciado en el puerto', PORT);
 });
+*/
+
+
+const cors = require('cors');
+// const routes = require('./routes')
+const dbConnect = require('./app/config/database')
+const port = process.env.PORT || 3000
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Routes
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+app.listen(port, () => {
+  console.log('Servidor iniciado en el puerto', port)
+})
+
+dbConnect;
---
+++ app/config/database.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+const config = require('./config');
+
+const sequelize = new Sequelize(
+  config[process.env.NODE_ENV || 'development']
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+module.exports = sequelize;
